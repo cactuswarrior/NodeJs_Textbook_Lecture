9899148dfd41fdbed40962275231bd9c96c908ed
천수승 <cheonss2357@gmail.com>
2022-02-22 00:32:05 +0900

    2022.02.21 3장 암호화까지

---
+++ 3장/var.js
@@ -0,0 +1,12 @@
+// const odd = '홀수입니다';
+// const even = '짝수입니다';
+
+// module.exports = {
+//   odd,
+//   even,
+// };
+
+// module.exports는 한번에 대입, exports는 각각 대입인점을 제외하고 동일하다
+// module.exports와 exports 차이점 > Node.js 교과서 p.104
+exports.odd = '홀수입니다';
+exports.even = '짝수입니다';
